480717b2ca367af210d7c27cee4f963a1dcbd5aa
Valentan390 <valentynhavrylenko8886@gmail.com>
2025-02-02 22:18:31 +0200

    Add NotFoundPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import SharedLayout from "./components/SharedLayout/SharedLayout";
 import PublicRoute from "./components/PublicRoute/PublicRoute";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
@@ -9,8 +9,15 @@ import EditShopPage from "./pages/EditShopPage/EditShopPage";
 import { useAppDispatch } from "./hooks";
 import { useEffect } from "react";
 import { currentThunk } from "./redux/authUser/authUserOperations";
+import ShopPage from "./pages/ShopPage/ShopPage";
+import AllMedicines from "./components/AllMedicines/AllMedicines.tsx";
+import DrugStore from "./components/DrugStore/DrugStore.tsx";
+import useCurrentUser from "./hooks/useCurrentUser.ts";
+import StatisticsPage from "./pages/StatisticsPage/StatisticsPage.tsx";
+import NotFound from "./pages/NotFound/NotFound.tsx";
 
 const App = () => {
+  const { isLoading } = useCurrentUser();
   const dispatch = useAppDispatch();
   useEffect(() => {
     dispatch(currentThunk());
@@ -19,17 +26,32 @@ const App = () => {
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
+        <Route
+          index
+          element={
+            <Navigate to={isLoading ? "/shop/create-shop" : "/register"} />
+          }
+        />
         <Route element={<PublicRoute />}>
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/login" element={<LoginPage />} />
         </Route>
 
         <Route element={<PrivateRoute />}>
-          <Route path="/shop">
-            <Route index element={<CreateShopPage />} />
+          <Route path="/shop" element={<ShopPage />}>
+            <Route path="create-shop" element={<CreateShopPage />} />
             <Route path="edit-shop" element={<EditShopPage />} />
           </Route>
+
+          <Route path="/medicine" element={<ShopPage />}>
+            <Route path="drugStore" element={<DrugStore />} />
+            <Route path="allMedicines" element={<AllMedicines />} />
+          </Route>
+
+          <Route path="/statistics" element={<StatisticsPage />} />
         </Route>
+
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   );
---
+++ frontend/src/components/DrugStore/DrugStore.tsx
@@ -0,0 +1,11 @@
+import {FC} from 'react';
+
+const DrugStore:FC = () => {
+    return (
+        <div>
+            DrugStore
+        </div>
+    );
+};
+
+export default DrugStore;
